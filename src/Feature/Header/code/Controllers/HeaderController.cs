bebc0f9139d9afc46305c6d1c5700cdf29c55c4c
Dominykas <Dominykas@PC787>
2022-05-09 10:45:45 +0300

    Add project files.

---
+++ src/Feature/Header/code/Controllers/HeaderController.cs
@@ -0,0 +1,21 @@
+﻿using BeerSorter.Feature.Header.Models;
+using System.Web.Mvc;
+
+namespace BeerSorter.Feature.Header.Controllers
+{
+    public class HeaderController : Controller
+    {
+        // GET: Header
+        public ActionResult Index()
+        {
+            var homeItem = Sitecore.Context.Database.GetItem(Templates.Home.HomeItemID);
+            var headerModel = new HeaderModel
+            {
+                Page = new MenuViewModel(homeItem),
+                LogoID = Templates.Header.Fields.LogoFieldID.ToString()
+            };
+
+            return View(headerModel);
+        }
+    }
+}
---
+++ src/Feature/Header/code/Templates.cs
@@ -0,0 +1,22 @@
+﻿using Sitecore.Data;
+
+namespace BeerSorter.Feature.Header
+{
+    public class Templates
+    {
+        public static class Home
+        {
+            public static readonly ID HomeItemID = new ID("{110D559F-DEA5-42EA-9C1C-8A5DF7E70EF9}");
+            public static readonly ID HomePageTemplatesID = new ID("{76036F5E-CBCE-46D1-AF0A-4143F9B557AA}");
+        }
+        public static class Header
+        {
+            public static class Fields
+            {
+                public static readonly ID LogoFieldID = new ID("{4C73CC87-77F3-4BF5-B52C-AEEA6FE69571}");
+            }
+
+        }
+
+    }
+}
